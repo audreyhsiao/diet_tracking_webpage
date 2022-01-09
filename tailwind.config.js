798425af36b2f7da73eddd188a5bf619a3d99167
audreyhsiao <51323445+audreyhsiao@users.noreply.github.com>
2022-01-09 18:34:03 +0800

    renew style

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       ...theme('colors'),
       'primary': '#faedc6',
       'beige':'#fff7dd',
-      'green1':'#7cd1b8',
+      'green1':'rgba(160,209,150)',
       'green2':'#3e817e',
       'orange':'#fabb51',
       
